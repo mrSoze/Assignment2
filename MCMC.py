ff46674d3c0ce9dacfb0b6d587bdcb33e422b668
Alessandro Folloni <kaiser_soze@Alessandros-MacBook-Pro.local>
2018-01-18 12:26:22 +0100

    MCMC sampler worksgit add .

--- MCMC.py
+++ MCMC.py
@@ -1,55 +1,112 @@
 #Metropolis-Hastings
-from SVDataGenerator import sv_parameter_generator as param_gen
-from SVDataGenerator import sv_data_generator as data_gen
+from SVDataGenerator2 import sv_parameter_generator as param_gen
+from SVDataGenerator2 import sv_data_generator as data_gen
 
 import numpy as np
+np.random.seed(1234)
 import math
 from scipy.stats import norm
 
 T=500
-PROPOSAL_EQUAL_CURRENT_STATE = "Proposal state and current state are equal"
 
-parameters = param_gen()
-phi = parameters[0]
-sigma = parameters[1]
-beta = parameters[2]
-observations, real_volatility = data_gen(parameters)
-
-def sample_proposal(t):
+def sample_proposal_calc_alpha(t):
     tmp = np.random.randint(1, 4)
     proposal_state = None
+    #hastings ratio is the ratio between g(x|x') and g(x'|x)
+    hastings_ratio = None
     if tmp == 1:
         proposal_state = first_proposal(t)
+        hastings_ratio = hastings_ratio_case_a(t, proposal_state)
     elif tmp == 2:
         proposal_state = second_proposal(t)
+        hastings_ratio = hastings_ratio_case_b(t, proposal_state)
     elif tmp == 3:
         proposal_state = third_proposal(t)
+        hastings_ratio = hastings_ratio_case_c(t, proposal_state)
+    #Stationary ratio is the ration between p*(x') and p*(x)
+    stationary_ratio = stationary_ratio_function(t, proposal_state)
+    alpha = stationary_ratio * hastings_ratio
+    return proposal_state, alpha
+
+def stationary_ratio_function(t, proposal_state):
+    current_state = real_volatility[t]
+    ratio = None
+    if t == 0:
+        next_state = real_volatility[t+1]
+        var = (sigma ** 2) / (1 - phi ** 2)
+        p_current_state = norm.pdf(current_state, loc=0, scale=np.sqrt(var))
+        p_current_to_next = norm.pdf(next_state, loc=phi*current_state, scale=sigma)
+        p_proposal_state = norm.pdf(proposal_state, loc=0, scale=np.sqrt(var))
+        p_proposal_to_next = norm.pdf(next_state, loc=phi*proposal_state, scale=sigma)
+        ratio = (p_proposal_state * p_proposal_to_next) / (p_current_state * p_current_to_next)
+    elif t == T:
+        past_state = real_volatility[T-1]
+        observation = observations[t]
+        p_past_to_current = norm.pdf(current_state, loc=phi*past_state, scale=sigma)
+        p_past_to_proposal = norm.pdf(proposal_state, loc=phi*past_state, scale=sigma)
+        p_observation_given_current = norm.pdf(observation, loc=0, scale=beta * math.exp(current_state / 2) )
+        p_observation_given_proposal = norm.pdf(observation, loc=0, scale=beta * math.exp(proposal_state / 2) )
+        ratio = (p_past_to_proposal * p_observation_given_proposal) / (p_past_to_proposal * p_observation_given_proposal)
     else:
-        raise ("random out of bound")
-    return proposal_state, tmp
+        next_state = real_volatility[t+1]
+        past_state = real_volatility[T-1]
+        observation = observations[t]
+        p_past_to_current = norm.pdf(current_state, loc=phi * past_state, scale=sigma)
+        p_past_to_proposal = norm.pdf(proposal_state, loc=phi * past_state, scale=sigma)
+        p_observation_given_current = norm.pdf(observation, loc=0, scale=beta * math.exp(current_state / 2))
+        p_observation_given_proposal = norm.pdf(observation, loc=0, scale=beta * math.exp(proposal_state / 2))
+        p_current_to_next = norm.pdf(next_state, loc=phi*current_state, scale=sigma)
+        p_proposal_to_next = norm.pdf(next_state, loc=phi*proposal_state, scale=sigma)
+        ratio_numerator = p_past_to_proposal * p_observation_given_proposal * p_proposal_to_next
+        ratio_denominator = p_past_to_current * p_observation_given_current * p_current_to_next
+        ratio = ratio_numerator / ratio_denominator
+    return ratio
+
+
+def hastings_ratio_case_c(t, proposal_state):
+    current_state = real_volatility[t]
+    if t == 0:
+        return 1
+    else:
+        p1 = math.exp(-current_state) - math.exp(-proposal_state)
+        p1 *= observations[t]**2
+        p1 /= 2 * beta**2
+        p2 = proposal_state - current_state
+        p2 /= 2
+        return math.exp(p1 + p2)
+
+def hastings_ratio_case_b(t, proposal_state):
+    current_state = real_volatility[t]
+    if t == T:
+        return 1
+    else:
+        next_state = real_volatility[t+1]
+        power = phi**2 * (proposal_state**2 - current_state**2) + 2*phi*next_state*(current_state - proposal_state)
+        power = power / (2 * sigma**2)
+        return math.exp(power)
+
+def hastings_ratio_case_a(t, proposal_state):
+    current_state = real_volatility[t]
+    if t == 0:
+        power = proposal_state ** 2 - current_state ** 2
+        var = (sigma ** 2) / (1-phi ** 2)
+        power /= 2 * var
+        return math.exp(power)
+    else:
+        past_state = real_volatility[t - 1]
+        power = proposal_state ** 2 - current_state ** 2 + 2 * phi * past_state * (current_state - proposal_state)
+        power = power / (2 * sigma ** 2)
+        return math.exp(power)
 
 def first_proposal(t):
     proposal = None
     if t == 0:
-        var = (parameters[1] ** 2) / (1-parameters[0] ** 2)
+        var = (sigma ** 2) / (1-phi ** 2)
         proposal = np.random.normal(scale=np.sqrt(var))
     else:
         v = np.random.normal()
-        proposal = parameters[0] * real_volatility[t-1] + parameters[1] * v
+        proposal = phi * real_volatility[t-1] + sigma * v
     return proposal
-
-def first_pdf(t, proposal):
-    pdf = None
-    if t>0:
-        normalization = 1 / sigma*math.sqrt(2*math.pi)
-        exp = proposal/sigma - phi*real_volatility[t-1]
-        exp = exp**2 / -2
-        pdf = normalization * math.exp(exp)
-    else:
-        normalization = 1 / sigma*math.sqrt(2*math.pi)
-        exp = proposal**2 / -2*sigma**2
-        pdf = normalization * math.exp(exp)
-    return pdf
 
 def second_proposal(t):
     proposal = None
@@ -60,18 +117,6 @@ def second_proposal(t):
         proposal = real_volatility[T]
     return proposal
 
-def second_pdf(t, proposal):
-    pdf = None
-    if t < T:
-        normalization = phi / sigma*math.sqrt(2*math.pi)
-        exp = ( real_volatility[t + 1] - phi*proposal ) / sigma
-        exp = exp ** 2 / -2
-        pdf = normalization * math.exp(exp)
-    else:
-        pdf = PROPOSAL_EQUAL_CURRENT_STATE
-    return pdf
-
-
 def third_proposal(t):
     proposal = None
     if t == 0:
@@ -81,56 +126,26 @@ def third_proposal(t):
         proposal = 2 * np.log( abs(observations[t]) / beta * abs(w) )
     return proposal
 
-def third_pdf(t, proposal):
-    pdf = None
-    if t == 0:
-        pdf = PROPOSAL_EQUAL_CURRENT_STATE
+def sampler(t):
+    # Sampling x'
+    proposal, alpha = sample_proposal_calc_alpha(t)
+    r = min(1, alpha)
+    u = np.random.uniform()
+    #new = None
+    if u < r:
+        new = proposal
     else:
-        normalization = abs(observations[t]) / (beta * math.sqrt(2*math.pi))
-        exp = (abs(observations[t])**2 / (2*beta**2))*math.exp(-proposal) - proposal/2
-        pdf = normalization * math.exp( exp )
-    return pdf
+        current = real_volatility[t]
+        new = current
+    return new
 
-def pdf_likelihood(t, state):
-    tmp = np.empty(T+1)
-    for i in range(T+1):
-        if i == 0:
-            loc = 0
-            scale = sigma**2 / (1 - phi**2)
-            tmp[0] = norm.pdf(state, loc=loc, scale=scale)
-        else:
-            transition = norm.pdf(state, loc=phi*real_volatility[t-1], scale=sigma**2)
-            likelihood = norm.pdf(state, loc=0, scale=abs(beta**2*math.exp(state)))
-            tmp[i] = transition * likelihood
-    return tmp
+parameters = param_gen()
+phi = parameters[0]
+sigma = parameters[1]
+beta = parameters[2]
+observations, real_volatility = data_gen(parameters)
+#observations = np.insert(observations, 0, np.NAN)
 
-
-state_chain=np.empty(len(real_volatility)+1)
-state_chain[0]=real_volatility[0]
-for t in range(T):
-    current_state=state_chain[t]
-    #Sampling x'
-    proposal, chosen_density=sample_proposal(t)
-    # Compute acceptance probability
-    stationary_proposal = pdf_likelihood(t, proposal)
-    stationary_current_state = pdf_likelihood(t, current_state)
-    ratio_proposal_current_st = np.prod( np.divide(stationary_proposal, stationary_current_state) )
-    print(ratio_proposal_current_st)
-
-    if chosen_density == 1:
-        #Correction in denominator
-        hastings_proposal = first_pdf(t, proposal)
-        #Correction in numerator
-        hastings_state = first_pdf(t, current_state)
-    if chosen_density == 2:
-        #Correction in denominator
-        hastings_proposal = second_pdf(t, proposal)
-        #Correction in numerator
-        hastings_state = second_pdf(t, current_state)
-    if chosen_density == 3:
-        #Correction in denominator
-        hastings_proposal = third_pdf(t, proposal)
-        #Correction in numerator
-        hastings_state = third_pdf(t, current_state)
-
-    alpha=None
+for i in range(5000):
+    t = np.random.randint(0, T+1)
+    mcmc = sampler(t)
